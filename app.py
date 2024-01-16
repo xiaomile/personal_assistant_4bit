6486f5d96420904b3e413ba1eaae1195e1234e30
xiaomile <15622388695@163.com>
2024-01-16 14:33:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ from openxlab.model import download
 import os
 
 download(model_repo='xiaomile/personal_assistant_4bit', output='xiaomile')
+
+os.system("lmdeploy convert  internlm-chat-7b /home/xlab-app-center/xiaomile --model-format awq --group-size 128")
+
+os.system("lmdeploy serve gradio /home/xlab-app-center/workspace")
