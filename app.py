34caa88a0ad8ccf14bf7b5107af7e0bdb4f132dc
xiaomile <15622388695@163.com>
2024-01-17 00:08:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from openxlab.model import download
 import os
+import subprocess
 
 download(model_repo='xiaomile/personal_assistant_4bit', output='xiaomile')
 
 os.system("lmdeploy convert  internlm-chat-7b /home/xlab-app-center/xiaomile --model-format awq --group-size 128")
 
-os.system("lmdeploy serve gradio /home/xlab-app-center/workspace")
+#os.system("lmdeploy serve gradio /home/xlab-app-center/workspace")
+
+cmd = "lmdeploy serve api_server /home/xlab-app-center/workspace --server_name 0.0.0.0 --server_port 23333 --instance_num 64 --tp 1"
+subprocess.call(cmd, creationflags=subprocess.CREATE_NEW_CONSOLE)
+os.system("lmdeploy serve gradio http://0.0.0.0:23333 --server_name 0.0.0.0 --server_port 7860 --restful_api True")
